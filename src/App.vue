f4fbe15d603b3974dcf98aa871704bc6d5c5f55b
11111 <norb662@gmail.com>
2017-08-29 16:10:11 -0400

    Added card layout and title

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,82 @@
 <template>
   <div id="app">
     <md-toolbar>
-      <h1 class="md-title">My Title</h1>
+      <h1 class ="md-title">{{ title }}</h1>
     </md-toolbar>
+    <div class="content">
+      <md-layout md-gutter=16>
+        <md-layout>
+          <md-card>
+            <md-card-header>
+              <div class="md-title">Title goes here</div>
+              <div class="md-subhead">Subtitle here</div>
+            </md-card-header>
 
-    <div class="main-content">
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo, rerum? Error sunt, aperiam dolores, atque expedita molestiae tenetur. Quis eveniet accusamus velit explicabo adipisci reiciendis modi eaque quas, officia excepturi.</p>
+            <md-card-content>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+            </md-card-content>
+
+            <md-card-actions>
+              <md-button>Action</md-button>
+              <md-button>Action</md-button>
+            </md-card-actions>
+          </md-card>
+        </md-layout>
+
+        <md-layout>
+          <md-card>
+            <md-card-header>
+              <div class="md-title">Title goes here</div>
+              <div class="md-subhead">Subtitle here</div>
+            </md-card-header>
+
+            <md-card-content>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+            </md-card-content>
+
+            <md-card-actions>
+              <md-button>Action</md-button>
+              <md-button>Action</md-button>
+            </md-card-actions>
+          </md-card>
+        </md-layout>
+
+        <md-layout>
+          <md-card>
+            <md-card-header>
+              <div class="md-title">Title goes here</div>
+              <div class="md-subhead">Subtitle here</div>
+            </md-card-header>
+
+            <md-card-content>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+            </md-card-content>
+
+            <md-card-actions>
+              <md-button>Action</md-button>
+              <md-button>Action</md-button>
+            </md-card-actions>
+          </md-card>
+        </md-layout>
+
+      </md-layout>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      title: 'Keep Clone'
+    }
+  }
 }
 </script>
 
 <style>
-.main-content {
-  padding: 16px;
+.content {
+  padding: 20px;
 }
 </style>
