f41052720a564d1a2abde76ae0978ea96f7c88d9
11111 <norb662@gmail.com>
2017-08-29 22:26:14 -0400

    Basic functionality

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,57 @@
   <div id="app">
     <md-toolbar>
       <h1 class ="md-title">{{ title }}</h1>
-    </md-toolbar>
-    <div class="content">
-      <md-layout md-gutter=16>
-        <md-layout v-for="note in notes"
-        v-bind:key="note.title">
-        <md-card md-with-hover>
-          <md-card-header>
-            <div class="md-title">{{ note.title }}</div>
-            <div class="md-subhead">Subtitle here</div>
-          </md-card-header>
+      <md-dialog md-open-from="#custom" md-close-to="#custom" ref="dialog1">
+        <md-dialog-title>Add New Note</md-dialog-title>
 
-          <md-card-content>
-            {{ note.body }}
-          </md-card-content>
+        <md-dialog-content>
+          <form>
+            <md-input-container>
+              <label>Title</label>
+              <md-textarea v-model="prompt.titleField"></md-textarea>
+            </md-input-container>
+          </form>
+          <form>
+            <md-input-container>
+              <label>Note</label>
+              <md-textarea v-model="prompt.bodyField"></md-textarea>
+            </md-input-container>
+          </form>
+        </md-dialog-content>
 
-          <md-card-actions>
-            <md-button>Action</md-button>
-            <md-button v-on:click="deleteNote(note)">Delete</md-button>
-          </md-card-actions>
-        </md-card>
-      </md-layout>
+        <md-dialog-actions>
+          <md-button class="md-primary" @click="closeDialog('dialog1')">Cancel</md-button>
+          <md-button class="md-primary" @click="closeDialog('dialog1')" v-on:click="addNote()">Ok</md-button>
+        </md-dialog-actions>
+      </md-dialog>
+
+      <md-button class="md-primary md-raised" id="custom" @click="openDialog('dialog1')">Add New Note</md-button>
+    </md-button>
+  </md-toolbar> <!-- Here is the cutoff -->
+  <div class="content">
+    <md-layout md-gutter=16>
+      <md-layout v-for="note in notes" v-bind:key="note.title">
+
+      <md-card md-with-hover>
+        <md-card-header>
+          <div class="md-title">{{ note.title }}</div>
+          <div class="md-subhead">Subtitle here</div>
+        </md-card-header>
+
+        <md-card-content>
+          {{ note.body }}
+        </md-card-content>
+
+        <md-card-actions>
+          <md-button>Action</md-button>
+          <md-button v-on:click="deleteNote(note)">Delete</md-button>
+        </md-card-actions>
+      </md-card>
 
     </md-layout>
-  </div>
+
+  </md-layout>
+</div>
 </div>
 </template>
 
@@ -55,38 +82,66 @@ export default {
   data () {
     return {
       title: 'Keep Clone',
-      notes:  []
+      notes:  [],
+      prompt: {
+        titleField: '',
+        bodyField: ''
+      }
     }
   },
   created() {
     this.updateNotes();
   },
   methods: {
-    addNotes() {
+    addNote() {
       let self = this;
-
+      console.log("Hello1");
+      noteClient.createNote({
+        title: self.prompt.titleField,
+        body: self.prompt.bodyField
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (response) {
+        console.log(error);
+      });
+      console.log('Hello');
     },
     deleteNote(note) {
       let self = this;
       noteClient.deleteNote(note.id)
-        .then(function (response) {
-          self.notes.splice(self.notes.indexOf(note), 1);
-          console.log(note.id);
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+      .then(function (response) {
+        self.notes.splice(self.notes.indexOf(note), 1);
+        console.log(note.id);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
     },
     updateNotes() {
       let self = this;
       noteClient.getAllNotes()
-        .then(function (response) {
-          self.notes = response.data;
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
+      .then(function (response) {
+        self.notes = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    },
+    openDialog(ref) {
+      this.$refs[ref].open();
+    },
+    closeDialog(ref) {
+      this.$refs[ref].close();
+    },
+    onOpen() {
+      console.log('Opened');
+    },
+    onClose(type) {
+      console.log('Closed', type);
     }
+
   }
 }
 </script>
